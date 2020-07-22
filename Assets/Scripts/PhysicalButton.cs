227b73a9af24cc063abf04ef38e7890daa0f4115
Kain Shepard <kainkun100@gmail.com>
2020-07-21 22:23:42 -0700

    buttons animated

--- Assets/Scripts/PhysicalButton.cs
+++ Assets/Scripts/PhysicalButton.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class PhysicalButton : Interactive
 {
+    Transform press;
+    float pressStartPosition;
+    public float animationSpeed = 1;
+    public float pressEndPosition;
+
+    private void Awake()
+    {
+        press = transform.GetChild(0);
+        pressStartPosition = press.localPosition.z;
+    }
+
     public override void Interact()
     {
         print("Pressed Button");
+        StopAllCoroutines();
+        StartCoroutine(pressButtonAnimation());
 
         base.Interact();
+    }
+
+    IEnumerator pressButtonAnimation()
+    {
+        while (press.localPosition.z > pressEndPosition)
+        {
+            press.localPosition -= new Vector3(0, 0, Time.deltaTime * animationSpeed);
+            yield return null;
+        }
+        while (press.localPosition.z < pressStartPosition)
+        {
+            press.localPosition += new Vector3(0, 0, Time.deltaTime * animationSpeed);
+            yield return null;
+        }
     }
 }
