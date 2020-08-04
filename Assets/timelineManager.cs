34a6e33b12930fcf32b625e7207175427f0fe763
Kain Shepard <kainkun100@gmail.com>
2020-08-04 14:48:39 -0700

    subtitle system

---
+++ Assets/timelineManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timelineManager : MonoBehaviour
+{
+
+    public Text subtitles;
+    [TextArea(5,10)]
+    public string[] subtitleStrings;
+    int currentSubtitleIndex;
+
+    void Start()
+    {
+        subtitles.text = "";
+        currentSubtitleIndex = 0;
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void NextSubtitles()
+    {
+        subtitles.text = subtitleStrings[++currentSubtitleIndex];
+    }
+
+    public void SetSubtitles(int i)
+    {
+        currentSubtitleIndex = i;
+        subtitles.text = subtitleStrings[currentSubtitleIndex];
+    }
+}
