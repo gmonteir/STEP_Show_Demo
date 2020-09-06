abf8cf5331373651606c9fee2538abb0f17b09cc
Kain Shepard <kainkun100@gmail.com>
2020-09-06 11:34:39 -0700

    updates

---
+++ Assets/ufo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ufo : MonoBehaviour
+{
+    public Animation anim;
+
+    public void playAnimation()
+    {
+        anim.Play();
+    }
+}
